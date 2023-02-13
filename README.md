54e22a7d686d7b99178c49404a1a1e5996a088e2
Karthik <mailmekarthik001@gmail.com>
2023-02-13 09:33:31 +0530

    add Files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # simple-mlflow-multistep-workflow
+
+### create and  activate environment 
+```
+sh init_pro.sh
+```
+
+### export environment file
+```
+conda env export > conda.yml
+```
---
+++ init_pro.sh
@@ -0,0 +1,4 @@
+conda create --prefix ./env python=3.7 -y
+source E:/anaconda/etc/profile.d/conda.sh # use your username instead of sunny
+source activate ./env
+pip install -r requirements.txt
